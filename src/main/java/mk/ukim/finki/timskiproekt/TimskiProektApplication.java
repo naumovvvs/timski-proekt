f4b83e9130976f5b6c7be00d378be3fb0b97ef4d
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2022-07-05 17:58:15 +0200

    project init

---
+++ src/main/java/mk/ukim/finki/timskiproekt/TimskiProektApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.timskiproekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimskiProektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimskiProektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/timskiproekt/TimskiProektApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.timskiproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimskiProektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
